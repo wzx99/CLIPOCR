0048517ea939c08666a865cc88667e41b524a17f
wzx99 <wzx99@mail.ustc.edu.cn>
2023-08-05 15:40:34 +0800

    first commit

--- train.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-
-python train.py dataset=synth trainer.gpus=2 ckpt_name=clipocr_synth model=clipocr model.batch_size=160 trainer.val_check_interval=1.0 model.lr=0.0014 trainer.max_epochs=20 +trainer.limit_train_batches=0.20496
-
-python test.py /path/to/ckpt/clipocr_synth/run/checkpoints --data_root path/to/data
-
